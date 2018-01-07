3fb6f1c499245e0262f8f898b844ea921596d3e3
Nicholas Vanderpool <nvanderp@MacBook-Pro.attlocal.net>
2018-01-07 18:25:10 -0500

    Redux complete

--- client/app.jsx
+++ client/app.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 
 ReactDOM.render(
-    <div>Hello, world!</div>,
+    <Provider store={store}>
+        {/* rest of your app goes here! */}
+    </Provider>,
     document.getElementById('app')
-)
+  );
---
+++ client/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+
+export default createStore(
+    rootReducer,
+    applyMiddleware(
+        thunkMiddleware,
+        createLogger()
+    )
+)
+
+export * from './reducers/dummy'
