10bca0857cef59b95a70d83ededc4a8c81f0205b
Nicholas Vanderpool <nvanderp@MacBook-Pro.attlocal.net>
2018-01-07 19:43:38 -0500

    small fix

--- server/apiRoutes/test.js
+++ server/apiRoutes/test.js
@@ -10,7 +10,7 @@ router.post('/', (req, res, next) => { res.send('POST request to Test made') })
 // matches PUT requests to /api/test/:testId
 router.put('/:testId', (req, res, next) => { res.send('PUT request to Test made') })
 
-// matches DELTE requests to /api/test/:testId
+// matches DELETE requests to /api/test/:testId
 router.delete('/:testId', (req, res, next) => { res.send('DELETE request to Test made') })
 
 module.exports = router
